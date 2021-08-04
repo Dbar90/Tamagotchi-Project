8b220117803d1c6aacee8802a5619681b9431903
Dbar90 <georilla90@yahoo.com>
2021-08-04 19:57:00 -0400

    changed/fixed some things and added button events

--- js/app.js
+++ js/app.js
@@ -5,8 +5,36 @@ class Tamagotchi {
     this.sleepiness = 1
     this.boredom = 1
     this.age = 0
+    this.timeAlive = 0
+    this.dead = false
+  }
+  feedPet() {
+    let button1 = document.querySelector('#feed')
+    button1.addEventListener('click', (event)=> {
+      this.hunger--
+      document.querySelector('#hunger').innerText = `${this.hunger}`
+    })
+  }
+  play() {
+    let button2 = document.querySelector('#play')
+    button2.addEventListener('click', (event)=> {
+      this.boredom--
+      document.querySelector('#boredom').innerText = `${this.boredom}`
+    })
+  }
+  goodNight() {
+    let button3 = document.querySelector('#lightsout')
+    button3.addEventListener('click', (event)=> {
+      this.sleepiness--
+      document.querySelector('#sleepiness').innerText = `${this.sleepiness}`
+    })
   }
 }
 
+
 const newTama = new Tamagotchi(name)
+console.log(newTama)
+newTama.feedPet()
+newTama.play()
+newTama.goodNight()
 console.log(newTama)
