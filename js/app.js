c50ed0310b32e1f05ae26da85de23bb1627ba902
Dbar90 <georilla90@yahoo.com>
2021-08-05 21:31:14 -0400

    added styling/changing theme of game

--- js/app.js
+++ js/app.js
@@ -4,14 +4,12 @@ class Tamagotchi {
     this.hunger = 1
     this.sleepiness = 1
     this.boredom = 1
-    this.age = 0
-    this.timeAlive = 0
-    this.dead = false
+    this.level = 1
   }
   feedPet() {
     let button1 = document.querySelector('#feed')
     button1.addEventListener('click', (event)=> {
-      this.hunger--
+      this.hunger-=2
       document.querySelector('#hunger').innerText = `${this.hunger}`
     })
   }
@@ -29,12 +27,58 @@ class Tamagotchi {
       document.querySelector('#sleepiness').innerText = `${this.sleepiness}`
     })
   }
+  gameTime() {
+    const metricsUp = setInterval(()=> {
+      this.hunger+=3
+      this.sleepiness++
+      this.boredom+=2
+      document.querySelector('#hunger').innerText = this.hunger
+      document.querySelector('#sleepiness').innerText = this.sleepiness
+      document.querySelector('#boredom').innerText = this.boredom
+      if (this.hunger >= 15) {
+        clearInterval(metricsUp)
+        alert(`${newTama} has starved to death!`)
+       }
+      if (this.sleepiness >= 15) {
+        clearInterval(metricsUp)
+        alert(`${newTama} has died from exhaustion!`)
+      }
+      if (this.boredom >= 15) {
+        clearInterval(metricsUp)
+        alert(`${newTama} has died of pure boredom!`)
+      }
+    }, 2000)
+   }
+  levelUp() {
+    const levelUp = setInterval(()=> {
+      this.level++
+      document.querySelector('#level').innerText = this.level
+      if (this.hunger >= 15) {
+        clearInterval(levelUp)
+      }
+      if (this.sleepiness >= 15) {
+        clearInterval(levelUp)
+      }
+      if (this.boredom >= 15) {
+        clearInterval(levelUp)
+      }
+    }, 5000)
+  }
+  evolve() {
+    if (this.level === 4) {
+      document.querySelector('#pic').setAttribute('src', "img1")
+    }
+  }
 }
 
 
 const newTama = new Tamagotchi(name)
-console.log(newTama)
+
+newTama.gameTime()
+newTama.levelUp()
 newTama.feedPet()
 newTama.play()
 newTama.goodNight()
+newTama.evolve()
+
 console.log(newTama)
