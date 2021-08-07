6243eb3f652d7e3e06f05f0cb0007309a7f22a4d
Dbar90 <georilla90@yahoo.com>
2021-08-07 10:11:33 -0400

    near done aside from image changing and movement being fixed

--- js/app.js
+++ js/app.js
@@ -10,21 +10,21 @@ class Tamagotchi {
     let button1 = document.querySelector('#feed')
     button1.addEventListener('click', (event)=> {
       this.hunger-=2
-      document.querySelector('#hunger').innerText = `${this.hunger}`
+      document.querySelector('#hunger').innerHTML = `${this.hunger}`
     })
   }
   play() {
     let button2 = document.querySelector('#play')
     button2.addEventListener('click', (event)=> {
       this.boredom--
-      document.querySelector('#boredom').innerText = `${this.boredom}`
+      document.querySelector('#boredom').innerHTML = `${this.boredom}`
     })
   }
   goodNight() {
     let button3 = document.querySelector('#lightsout')
     button3.addEventListener('click', (event)=> {
       this.sleepiness--
-      document.querySelector('#sleepiness').innerText = `${this.sleepiness}`
+      document.querySelector('#sleepiness').innerHTML = `${this.sleepiness}`
     })
   }
   gameTime() {
@@ -32,27 +32,27 @@ class Tamagotchi {
       this.hunger+=3
       this.sleepiness++
       this.boredom+=2
-      document.querySelector('#hunger').innerText = this.hunger
-      document.querySelector('#sleepiness').innerText = this.sleepiness
-      document.querySelector('#boredom').innerText = this.boredom
+      document.querySelector('#hunger').innerHTML = this.hunger
+      document.querySelector('#sleepiness').innerHTML = this.sleepiness
+      document.querySelector('#boredom').innerHTML = this.boredom
       if (this.hunger >= 15) {
         clearInterval(metricsUp)
-        alert(`${newTama} has starved to death!`)
+        alert(`${document.querySelector('#name').innerHTML} has starved to death!`)
        }
       if (this.sleepiness >= 15) {
         clearInterval(metricsUp)
-        alert(`${newTama} has died from exhaustion!`)
+        alert(`${document.querySelector('#name').innerHTML} has died from exhaustion!`)
       }
       if (this.boredom >= 15) {
         clearInterval(metricsUp)
-        alert(`${newTama} has died of pure boredom!`)
+        alert(`${document.querySelector('#name').innerHTML} has died of pure boredom!`)
       }
     }, 2000)
    }
   levelUp() {
     const levelUp = setInterval(()=> {
       this.level++
-      document.querySelector('#level').innerText = this.level
+      document.querySelector('#level').innerHTML = this.level
       if (this.hunger >= 15) {
         clearInterval(levelUp)
       }
@@ -62,23 +62,55 @@ class Tamagotchi {
       if (this.boredom >= 15) {
         clearInterval(levelUp)
       }
-    }, 5000)
+    }, 1000)
   }
   evolve() {
     if (this.level === 4) {
-      document.querySelector('#pic').setAttribute('src', "img1")
+      document.getElementById('pic').src = "image/charmeleon.png"
     }
+  }
+  movePoke() {
+    let picMove = document.getElementById('pic')
+    picMove.height = 60
+    picMove.width = 60
+    setInterval(()=> {
+    picMove.style.top = Math.floor((Math.random() * 100) + 1)+'px'
+    picMove.style.left = Math.floor((Math.random() * 100) + 1)+'px'
+  }, 300)
   }
 }
 
+let buttonSubmit = document.querySelector('#submit')
+buttonSubmit.addEventListener('click', (event)=> {
+  event.preventDefault()
+  let pokeName = document.getElementById('input').value
+  document.querySelector('h2').innerHTML = pokeName
+})
 
-const newTama = new Tamagotchi(name)
 
-newTama.gameTime()
-newTama.levelUp()
-newTama.feedPet()
-newTama.play()
-newTama.goodNight()
-newTama.evolve()
+const newTama = new Tamagotchi()
+
+
+  let startGame = document.querySelector('#start')
+  startGame.addEventListener('click', (event)=> {
+    // newTama.gameTime()
+    newTama.levelUp()
+    // newTama.feedPet()
+    // newTama.play()
+    // newTama.goodNight()
+    newTama.evolve()
+    newTama.movePoke()
+  })
+
+
+
+
+
+// newTama.gameTime()
+// newTama.levelUp()
+// newTama.feedPet()
+// newTama.play()
+// newTama.goodNight()
+// newTama.evolve()
 
 console.log(newTama)
